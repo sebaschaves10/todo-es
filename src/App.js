6d36f2f51d947f6221e2f2c97d13e29d92ae70ee
Sebastian Chaves <sebaxtian.chaves@gmail.com>
2024-01-10 21:08:02 -0500

    feat: agregamos dark-mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [darkMode, setDarkMode] = useState("dark");
+
+  function toggleDarkMode() {
+    if (darkMode === "dark") {
+      setDarkMode("light");
+      console.log(darkMode);
+    } else {
+      setDarkMode("dark");
+      console.log(darkMode);
+    }
+  }
+
   return (
-    <div>
-      <h1 className="font-extrabold">React App</h1>
+    <div className={`${darkMode === "dark" ? "dark" : "light"} min-h-screen flex flex-col`}>
+      <div className="bg-zinc-100 dark:bg-gray-900 flex-auto text-gray-900 dark:text-white">
+        <h1 className="font-extrabold">React App</h1>
+        <button onClick={toggleDarkMode}>
+          <span>Toggle Dark Mode</span>
+        </button>
+      </div>
     </div>
   );
 }
